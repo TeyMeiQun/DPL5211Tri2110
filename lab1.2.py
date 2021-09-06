d4a0668bf02d8aa79eea05f07cdef5caea4d349c
TeyMeiQun <90165835+TeyMeiQun@users.noreply.github.com>
2021-09-06 10:40:14 +0800

    Add files via upload

---
+++ lab1.2.py
@@ -0,0 +1,12 @@
+#lab1.2 get unput from two numbers and display the sum of two numbers
+sum1=int(input("enter number 1:  "))
+sum2=int(input("enter number 2:  "))
+total=sum1+sum2
+print("num1 +num2=",total)
+
+#print("the total number is",total)
+#the sum of 5 and 6 is 11
+
+print("the sum of",sum1,"and",sum2,"=",total)
+
+print("the sum of {} and {} is {}".format(sum1,sum2,total))
